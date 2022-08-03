d34b915dcab8154f93a9d4b806748358ebde1ef5
MendingSalto <108483735+MendingSalto@users.noreply.github.com>
2022-08-03 18:22:24 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
-*sᴛᴀᴛᴜs:* single 
+*ɴᴀᴍᴇ:* ®.KoKo•Rey
+*ᴀɢᴇ:* 19ᵗʰ
+*sᴛᴀᴛᴜs:* DiniPtr 
 
 *ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bogor
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
